9bc8e066794756df65ea1fdceb4604215ee1ed39
RuinedVLR <rzhurakhov@gmail.com>
2026-03-02 14:57:16 -0400

    final result (idle and movement blend trees made)

--- Assets/Systems/PlayerController.cs
+++ Assets/Systems/PlayerController.cs
@@ -8,22 +8,30 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D playerRb;
 
+    Animator animController;
 
+    private int xPosHash = Animator.StringToHash("Xpos");
+    private int yPosHash = Animator.StringToHash("Ypos");
 
+    private int moveInputXHash = Animator.StringToHash("MoveInputX");
+    private int moveInputYHash = Animator.StringToHash("MoveInputY");
+    private int isMovingHash = Animator.StringToHash("IsMoving");
 
-
+    private int lastMoveXHash = Animator.StringToHash("LastMoveX");
+    private int lastMoveYHash = Animator.StringToHash("LastMoveY");
 
     private void Awake()
     {
         playerRb = GetComponent<Rigidbody2D>();
         if (playerRb == null) Debug.LogError("Rigidbody2D component not found on the player object.");
 
+        animController = GetComponentInChildren<Animator>();
     }
 
 
     private void Update()
     {
-
+        HandlePlayerAnimations();
     }
 
     private void FixedUpdate()
@@ -46,7 +54,26 @@ public class PlayerController : MonoBehaviour
         playerRb.MovePosition(playerRb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
     }
 
+    private void HandlePlayerAnimations()
+    {
+        if (moveInput != Vector2.zero)
+        {
+            animController.SetFloat(lastMoveXHash, animController.GetFloat(moveInputXHash));
+            animController.SetFloat(lastMoveYHash, animController.GetFloat(moveInputYHash));
+            
+            animController.SetFloat(moveInputXHash, moveInput.x);
+            animController.SetFloat(moveInputYHash, moveInput.y);
 
+            animController.SetBool(isMovingHash, true);
+        }
+        else
+        {
+            animController.SetFloat(moveInputXHash, moveInput.x);
+            animController.SetFloat(moveInputYHash, moveInput.y);
+
+            animController.SetBool(isMovingHash, false);
+        }
+    }
 
 
 
